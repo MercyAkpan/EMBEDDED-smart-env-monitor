0b00533e28169ce897be87be4fce32e0463798a8
MercyAkpan <akpanmeme@gmail.com>
2025-10-11 15:51:43 +0100

    Updated my PIR prototype file

--- src/sensors/PIR_prototype.cpp
+++ src/sensors/PIR_prototype.cpp
@@ -5,27 +5,17 @@
 // #include <DallasTemperature.h>
 
 //
-const int oneWireBus = 15;
+// const int oneWireBus = 15;
 // OneWire oneWire(oneWireBus);
 // DallasTemperature sensors(&oneWire);
  //
-//----Set GPIOs for LED and PIR Motion Sensor
-const int led = 23;
-const int PIRSensor = 4;
-// -----Timer: Auxiliary variables
-#define timeSeconds 100
-unsigned long now = millis();
-unsigned long lastTrigger = 0;
-unsigned long lastBlink = 0;
-unsigned long blinkNow = millis();
-boolean motionDetectedFlag = false;
-boolean startTimer = false;
+
 //---Checks if motion was detected, sets LED HIGH and starts a timer
 void IRAM_ATTR detectsMovement(void)
 {
-    Serial.println("PIRpinValue:");
-    Serial.println(digitalRead(PIRSensor));
-    Serial.println("Motion DETECTED......");
+    // Serial.println("PIRpinValue:");
+    // Serial.println(digitalRead(PIRSensor));
+    // Serial.println("Motion DETECTED......");
     motionDetectedFlag = true;
 }
 // Blink LED
@@ -40,48 +30,4 @@ void blinkLedOff()
     digitalWrite(led, LOW);
     lastBlink = millis();
 }
-void setup()
-{
-    Serial.begin( 115200 ); // Serial port for debugging purposes
-    pinMode( PIRSensor, INPUT_PULLUP); // PIR Motion Sensor mode INPUT_PULLUP
-    pinMode( led, OUTPUT );
-    digitalWrite( led, LOW );
-    // Set PIRSensor pin as interrupt, assign interrupt function and set RISING mode
-    attachInterrupt( digitalPinToInterrupt(PIRSensor), detectsMovement, RISING); 
-    // sensors.begin();
-    Serial.println("Setup complete");
-}
-void loop()
-{
-      // Serial.println(digitalRead(PIRSensor));
-      // delay(500);
-    if (motionDetectedFlag && !startTimer)
-    {
-      motionDetectedFlag = false;
-      Serial.println( " MOTION DETECTED " );
-      Serial.println("Turning ON the LED");
-      startTimer = true;
-      lastTrigger = millis();
-    }
-    if (startTimer && ((millis() - lastBlink) > timeSeconds))
-    {
-      int pinState = digitalRead(led);
-      if (pinState == 0)
-      {
-         blinkLedOn();
-      } 
-      else
-      {
-        blinkLedOff();
-      }
-    }
-    now = millis();
-    if( startTimer && (now - lastTrigger > ( timeSeconds*50)))
-    {
-        Serial.println("===========================");
-        Serial.println("In here now");
-        Serial.println(" Turning OFF the LED " );
-        digitalWrite( led, LOW );
-        startTimer = false;
-    }
-}
+
