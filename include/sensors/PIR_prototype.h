d3cf8e3044cdeb7c78dd60de30aad814f59241fa
MercyAkpan <akpanmeme@gmail.com>
2025-10-11 15:50:03 +0100

    Updated the PIR prototype header file

--- include/sensors/PIR_prototype.h
+++ include/sensors/PIR_prototype.h
@@ -5,5 +5,19 @@
 #include <Arduino.h>
 // void IRAM_ATTR myGlobalISR(void); // Declaration of a global IRAM_ATTR function
 void IRAM_ATTR detectsMovement(void);
+void blinkLedOff();
+void blinkLedOn();
+
+//----Set GPIOs for LED and PIR Motion Sensor
+const int led = 23;
+const int PIRSensor = 4;
+// -----Timer: Auxiliary variables
+#define timeSeconds 100
+extern unsigned long now;
+extern unsigned long lastTrigger;
+extern unsigned long lastBlink;
+extern unsigned long blinkNow;
+extern boolean motionDetectedFlag;
+extern boolean startTimer;
 
 #endif // MY_FUNCTIONS_H
